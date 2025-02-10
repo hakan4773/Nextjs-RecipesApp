22a3afb5348570251bb4ac4d1aec5c5ac8a4763a
Coders Club <hi@codersclub.com>
2025-02-10 21:52:23 +0300

    update button

--- food-app/src/app/page.js
+++ food-app/src/app/page.js
@@ -6,17 +6,22 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="relative flex flex-col  ">
+    <div className=" flex flex-col  ">
+       <div className="relative"> 
+        
         <Image
-          className="w-full h-[500px]  opacity-90"
+          className="w-full h-[500px] "
           src={menu}
           alt="food"
-          width={1000}
+          width={800}
           height={500}
         />
-<div  className="absolute top-40 flex  flex-col left-1/3  space-y-8 items-center text-center">
-<h1 className="text-4xl    shadow-2xl text-white">Hemen Yolculuğa Başlayın</h1>
-<Link href="/foods"  className="border p-1 w-40 rounded-md text-black bg-white  hover:text-2xl shadow-2xl text-2xl text-bold">Start</Link>
+
+
+
+<div  className="absolute  flex  flex-col top-1/3 left-1/3 w-80  justify-center    items-center text-center">
+<Link href="/foods"  className=" border p-1 rounded-md text-white   hover:text-4xl shadow-2xl text-4xl text-bold">Hemen Yolculuğa Başlayın</Link>
+</div>
 </div>
 <Contents />
 
