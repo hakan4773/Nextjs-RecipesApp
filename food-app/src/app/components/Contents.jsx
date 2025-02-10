2f3c3daed217f2e0059b42a2666b815bb45ab269
hakan4773 <hb480312@gmail.com>
2025-02-11 00:45:41 +0300

    update css 1

--- food-app/src/app/components/Contents.jsx
+++ food-app/src/app/components/Contents.jsx
@@ -15,47 +15,42 @@ const Contents = () => {
 
       <Stack direction="column" sx={{mt:4}} >
         <Stack direction="row" sx={{mx:2}}>
-          <Image src={recipe} width={200} height={100} alt="recipe" className="rounded-md p-2" />
-          <p><span className="font-bold text-xl mx-2 ">1- Yemek Tarifleriyle Lezzetli Anlar</span>
-            Yemek yapmak, sadece bir ihtiyaç
-            değil, aynı zamanda bir sanat ve tutkudur. Her tarif, farklı
-            kültürlerden bir hikaye taşır ve mutfağınıza yepyeni lezzetler
-            getirir. Pratik Tarifler: Günlük hayatın koşuşturmasında hızlı ama
+          <Image src={recipe} 
+        
+            alt="recipe" className="rounded-md p-2 lg:h-64 lg:w-1/2  h-64" />
+          <p className="lg:text-xl text-sm w-full"><span className="font-bold lg:text-2xl text-sm mx-2 ">
+            1- Yemek Tarifleriyle Lezzetli Anlar</span><br></br>
+           Pratik Tarifler: Günlük hayatın koşuşturmasında hızlı ama
             lezzetli yemekler hazırlamak ister misiniz? 15 dakikada
             hazırlanabilecek makarna soslarından, kolay çorba tariflerine kadar
-            pek çok seçenek mevcut. Geleneksel Lezzetler: Anne tariflerinden
-            esinlenen yemeklerle nostaljik bir yolculuğa çıkabilirsiniz. Bir
-            kasede sıcacık mercimek çorbası ya da fırından yeni çıkmış
-            böreklerle sofranıza samimi bir hava katın. Tatlı Molası: Tatlı
-            severler için hafif sütlü tatlılardan çikolatalı keklere kadar geniş
-            bir yelpaze var. Yemekten sonra kendinizi şımartmayı unutmayın!
+            pek çok seçenek mevcut.
             Mutfağa adım atmak için geç kalmayın; kendi tariflerinizi keşfedin
             ve paylaşmanın keyfini çıkarın. Her yemek, sevdiklerinizle
             geçirdiğiniz anları tatlandırır. Şimdi, en sevdiğiniz tarifi seçin
             ve mutfağın büyülü dünyasına dalın! Yemek Tariflerine Göz atmayı Unutmayın.😊
+            <br></br>
         <Link href={"/foods"} className="mx-2  p-1 bg-none rounded-full border">  <ArrowForwardIcon ></ArrowForwardIcon></Link>
           </p>
 
         </Stack>
 
         <Stack direction="row" sx={{mx:2}}>
-          <Image src={diet} width={200} height={100} alt="recipe" className="rounded-md p-2" />
-          <p><span className="font-bold text-xl mx-2 ">2- Diyet Yaparken Sağlıklı ve Dengeli Beslenme
-          </span>
+          <Image src={diet}  alt="recipe" className="rounded-md p-2 lg:h-64 lg:w-1/2  h-64" />
+          <p className="lg:text-xl text-sm w-full"><span className="font-bold lg:text-2xl text-sm mx-2 ">2- Diyet Yaparken Sağlıklı ve Dengeli Beslenme
+          </span>   <br></br>
           Diyet yapmak, yalnızca kilo vermek değil, sağlıklı bir yaşam tarzı benimsemek için harika bir adımdır. Doğru bir diyet programıyla hem kendinizi enerjik hissedebilir hem de vücudunuzun ihtiyaç duyduğu besinleri alabilirsiniz.
           Diyet sürecinde ufak kaçamaklar yapabilirsiniz, bu doğaldır. Önemli olan dengeli bir şekilde yolunuza devam etmektir. Hedeflerinizi hatırlayın, kendinizi ödüllendirin ve süreci bir yaşam tarzı haline getirin.
-
-Unutmayın, diyet yapmak sadece fiziksel değil, zihinsel sağlığınızı da destekler. Sağlıklı beslenerek hem bedeninizi hem de ruhunuzu mutlu edin! 😊
+          Unutmayın, diyet yapmak sadece fiziksel değil, zihinsel sağlığınızı da destekler. Sağlıklı beslenerek hem bedeninizi hem de ruhunuzu mutlu edin! 😊
 
 <Link href={"/diet"} className="mx-2 p-1 bg-none rounded-full border">  <ArrowForwardIcon ></ArrowForwardIcon></Link>
 
           </p>
         </Stack>
         <Stack direction="row" sx={{mx:2}}>
-          <Image src={meal} width={200} height={100} alt="recipe" className="rounded-md p-2" />
-          <p><span className="font-bold text-xl mx-2 ">3- Öğünler ve Doğru Beslenme
+          <Image src={meal} alt="recipe" className="rounded-md p-2 lg:h-64 lg:w-1/2  h-64" />
+          <p className="lg:text-xl text-sm w-full"><span className="font-bold lg:text-2xl text-sm mx-2 ">3- Öğünler ve Doğru Beslenme
          
-          </span>
+          </span>   <br></br>
            <b>Kahvaltı</b>: Güne Enerjik Başlangıç
           Kahvaltı, günün en önemli öğünüdür. Gece boyunca dinlenen vücut, enerjiye ihtiyaç duyar.
           <b>Öğle Yemeği</b>: Enerjiyi Koruma Zamanı
@@ -73,16 +68,16 @@ Unutmayın, diyet yapmak sadece fiziksel değil, zihinsel sağlığınızı da d
         </Stack>
 
         <Stack direction="row" sx={{mx:2}}>
-          <Image src={diet} width={200} height={100} alt="recipe" className="rounded-md p-2" />
-          <p><span className="font-bold text-xl mx-2 ">4- Dünya Mutfakları: Kültürlerin Lezzet Yolculuğu
+          <Image src={diet}  alt="recipe" className="rounded-md p-2 lg:h-64 lg:w-1/2  h-64" />
+          <p className="lg:text-xl text-sm w-full"><span className="font-bold lg:text-2xl text-sm mx-2 ">4- Dünya Mutfakları: Kültürlerin Lezzet Yolculuğu
 
          
-          </span>
+          </span>   <br></br>
           Her ülkenin mutfağı, kültürünün ve tarihinin bir yansımasıdır. Baharatların kokusundan, pişirme yöntemlerine kadar her detay, bir hikaye anlatır. 
           İşte dünyanın en sevilen mutfaklarından bazıları ve öne çıkan lezzetleri:
           İtalyan mutfağı,Türk mutfağı,Meksika mutfağı,Asya mutfağı,Orta Doğu Mutfağı
           Dünya mutfakları, insanların kültürlerini keşfetmenin en lezzetli yollarından biridir. Yeni tarifler deneyerek ya da bu mutfaklara özgü restoranları ziyaret ederek, dünyanın dört bir yanını tabaklarınızda keşfetmeye ne dersiniz? 🌍🍴
-          <Link href={"/foods"} className="mx-2 p-1 bg-none rounded-full border">  <ArrowForwardIcon ></ArrowForwardIcon></Link>
+          <br></br> <Link href={"/foods"} className="mx-2 p-1 bg-none rounded-full border">  <ArrowForwardIcon ></ArrowForwardIcon></Link>
 
           </p>
         </Stack>
